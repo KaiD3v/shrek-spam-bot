c73022a0220698b029fbd049dcce18033cecdc34
kaid3v <kaikricardo99@gmail.com>
2026-05-23 19:21:37 -0300

    remove

--- src/utils/commands-array.js
+++ src/utils/commands-array.js
@@ -1,7 +1,5 @@
 import { enviarCommand } from "../commands/enviar.js";
-import { penisCommand } from "../commands/penis.js";
 import { pingCommand } from "../commands/ping.js";
 import { shrekCommand } from "../commands/shrek.js";
-import { testeCommand } from "../commands/teste.js";
 
-export const commands = [enviarCommand, penisCommand, pingCommand, shrekCommand, testeCommand];
+export const commands = [enviarCommand, pingCommand, shrekCommand];
